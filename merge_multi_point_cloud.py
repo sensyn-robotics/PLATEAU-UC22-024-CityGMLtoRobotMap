a4ecb1d536acca988a6d56c0126e5ba36f00d359
Akifumi Ohata <mojanekonihiki@gmail.com>
2023-02-23 20:30:16 +0900

    fix bug

--- merge_multi_point_cloud.py
+++ merge_multi_point_cloud.py
@@ -14,25 +14,32 @@ if __name__ == '__main__':
     parser.add_argument('--save_dir', type=str, default=str(Path.home().joinpath('CG2RM', 'pointcloud')))
     args = parser.parse_args()
     input_files = [Path(file_str) for file_str in args.point_cloud_files]
-
+    print(input_files)
     if len(input_files) < 2:
 
         sys.exit()
 
     pc_list = [o3d.io.read_point_cloud(str(file_path)) for file_path in input_files]
+    color_point_list =[]
     point_cloud_base = None
 
     for pc in pc_list:
         pc.paint_uniform_color([random.random(), random.random(), random.random()])
+        color_point_list.append(pc)
 
-        pc_load = np.asarray(pc.points)
-        pc_color = np.asarray(pc.colors)
 
-        if point_cloud_base is not None:
-            p_load = np.concatenate((np.asarray(point_cloud_base.points), pc_load), axis=0)
-            p_color = np.concatenate((np.asarray(point_cloud_base.colors), pc_color), axis=0)
-        else:
-            point_cloud_base = pc
+
+        # pc_load = np.asarray(pc.points)
+        # pc_color = np.asarray(pc.colors)
+        #
+        # if point_cloud_base is not None:
+        #     p_load = np.concatenate((np.asarray(point_cloud_base.points), pc_load), axis=0)
+        #     p_color = np.concatenate((np.asarray(point_cloud_base.colors), pc_color), axis=0)
+        # else:
+        #     point_cloud_base = pc
+
+    p_load = np.concatenate([np.asarray(pc.points)  for pc in color_point_list], axis=0)
+    p_color = np.concatenate([np.asarray(pc.colors)  for pc in color_point_list], axis=0)
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(p_load)
